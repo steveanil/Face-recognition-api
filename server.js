07c88e01ef1cc110b4754f836e2e5b6c4b5dbf70
Steve <steveanil2003@gmail.com>
2023-08-11 17:40:54 -0700

    security fix

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)} )
 // detect face with Clarifai API
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)} )
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`App is running on port ${process.env.PORT}`)
+app.listen(3000, () => {
+  console.log(`App is running on port 3000`)
 }); //Port 3000
 
 
